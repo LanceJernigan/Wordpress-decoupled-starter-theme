4f4f74e674c516ed4b050359ed11720f0bb6c3ae
Lance Jernigan <ljernigan@pyxl.com>
2018-03-23 00:03:05 -0400

    setup app enqueue

---
+++ classes/Enqueues/App.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Decouple\Enqueues;
+
+class App
+{
+
+    public static function init()
+    {
+        $class = new self;
+        add_action('wp_enqueue_scripts', [$class, 'register']);
+    }
+
+    public function register()
+    {
+        $js_locator = 'dist/scripts/index.js';
+        wp_register_script(
+            'App',
+            $js_locator,
+            false,
+            $js_locator,
+            true
+        );
+    }
+}
--- functions.php
+++ functions.php
@@ -8,5 +8,5 @@ define(__NAMESPACE__ . '\URI', plugin_dir_url(__FILE__));
 require_once 'lib/autoload.php';
 
 add_action('plugins_loaded', function () {
-
+    Enqueues\App::init();
 });
