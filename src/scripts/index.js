cc70668c8062aaaa4029b68a8e4a3e19424e763f
Lance Jernigan <ljernigan@pyxl.com>
2018-03-23 00:30:32 -0400

    initial App component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,9 @@
-console.log( 'ayyyyeeee' );
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => <h1>Hello world!</h1>
+
+render(
+    <App />,
+    document.getElementById('root')
+);
