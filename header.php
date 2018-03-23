7af219657b47381a07be377ef6fa7d47827c63d9
Lance Jernigan <ljernigan@pyxl.com>
2018-03-22 23:33:56 -0400

    added header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html  <?php language_attributes(); ?> class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
+<head>
+
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Site Title -->
+    <title><?php wp_title() ?></title>
+
+</head>
+<body <?php body_class() ?>>
